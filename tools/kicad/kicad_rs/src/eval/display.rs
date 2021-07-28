e6da60e90b3d0094b5b3770fac588fbbadf0d02d
Dennis Marttinen <twelho@welho.tech>
2021-07-28 18:24:52 +0300

    Assess review feedback, use `gpoint` for significant digits

--- tools/kicad/kicad_rs/src/eval/display.rs
+++ tools/kicad/kicad_rs/src/eval/display.rs
@@ -1,8 +1,9 @@
 use evalexpr::Value;
+use gpoint::GPoint;
 use std::fmt::{Display, Formatter, Result};
 
-const UNITS_LARGE: &[char] = &['k', 'M', 'G', 'T', 'E'];
-const UNITS_SMALL: &[char] = &['m', 'µ', 'n', 'p'];
+const UNITS_LARGE: &[char] = &['k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
+const UNITS_SMALL: &[char] = &['m', 'µ', 'n', 'p', 'f', 'a', 'z', 'y'];
 
 pub struct PrettyPrintValue<'a> {
     value: &'a Value,
@@ -34,7 +35,8 @@ impl<'a> Display for PrettyPrintValue<'a> {
                     }
                 }
 
-                write!(f, "{:.2}", float)?; // Fixed two-decimal precision
+                // Use dynamic precision with at most three significant digits
+                write!(f, "{:.3}", GPoint(float))?;
                 if self.unit.is_some() {
                     write!(f, " ")?;
                 }
@@ -59,9 +61,11 @@ impl<'a> Display for PrettyPrintValue<'a> {
 mod tests {
     use super::*;
 
-    macro_rules! test_map {
+    macro_rules! test_ppv {
         (($value:expr, $unit:expr, $target:expr)) => {
-            assert_eq!(PrettyPrintValue::new(&$value.into(), &Some($unit.into())).to_string(), $target);
+            assert_eq!(PrettyPrintValue::new(
+                &$value.into(), &Some($unit.into())
+            ).to_string(), $target);
         };
 
         (($value:expr, $target:expr)) => {
@@ -71,39 +75,35 @@ mod tests {
         ($name:ident, $($x:tt),+) => {
             #[test]
             fn $name() {
-                $(test_map!($x);)*
+                $(test_ppv!($x);)*
             }
         };
     }
 
-    test_map!(ppv_integer, (10000, "pcs.", "10000 pcs."), (100, "100"));
-    test_map!(
+    test_ppv!(ppv_integer, (10000, "pcs.", "10000 pcs."), (100, "100"));
+    test_ppv!(
         ppv_string,
         ("test", "quantity", r#""test" quantity"#),
         ("test", r#""test""#)
     );
-    test_map!(
+    test_ppv!(
         ppv_float1,
-        (0.0, "F", "0.00 F"),
+        (0.0, "F", "0 F"),
         (3.01, "F", "3.01 F"),
-        (9.999, "€", "10.00 €")
+        (9.999, "€", "10 €")
     );
-    test_map!(
-        ppv_float2,
-        (0.0, "0.00"),
-        (3.0051, "3.01"),
-        (9.999, "10.00")
-    );
-    test_map!(
+    test_ppv!(ppv_float2, (0.0, "0"), (3.0051, "3.01"), (9.999, "10"));
+    test_ppv!(
         ppv_float_large,
-        (12345.0, "C", "12.35 kC"),
-        (12345.0, "12.35k"),
-        (3e18, "3000.00E")
+        (12345.0, "C", "12.3 kC"),
+        (12355.0, "C", "12.4 kC"),
+        (12345.0, "12.3k"),
+        (3e27, "3e+03Y")
     );
-    test_map!(
+    test_ppv!(
         ppv_float_small,
-        (123e-6, "R", "123.00 µR"),
+        (123e-6, "R", "123 µR"),
         (0.00123456, "1.23m"),
-        (3e-15, "0.00p")
+        (3e-27, "0.003y")
     );
 }
