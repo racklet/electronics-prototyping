99fcc4d168e3f355bd19e0f21cb90d9e57cfc3e5
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2021-12-11 14:53:25 +0200

    Reorganize fusesoc libraries
    * Create a new directory for fusesoc workspace
    * Automate fusesoc workspace library initialization
    * Create a folder for racklet cores as a preparation
      for maybe extracting them to a separate repository
    * Add diagrams
    * Track logic analyzer captures to git LFS

---
+++ fusesoc-workspace/init_workspace.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# clone dependencies
+fusesoc library add fusesoc-cores https://github.com/fusesoc/fusesoc-cores
+fusesoc library add sd_device https://github.com/fusesoc/sd_device.git
+fusesoc library add racklet-cores racklet-cores
