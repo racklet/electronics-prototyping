08e5c5d2286e627255b72a4b5dd696b1f52b33e3
Verneri Hirvonen <verneri.hirvonen@aalto.fi>
2022-01-04 19:01:51 +0200

    Add spi flash simulation model

--- fusesoc-workspace/racklet-cores/spi_flash_ctrl/tb/spi_flash_ctrl_tb.v
+++ fusesoc-workspace/racklet-cores/spi_flash_ctrl/tb/spi_flash_ctrl_tb.v
@@ -11,10 +11,31 @@ reg btn = 1;
 
 reg zero = 0;
 
+wire flash_cs;
+wire flash_clk;
+wire flash_mosi;
+wire flash_miso;
+wire flash_wp_n;
+wire flash_reset_n;
+assign flash_wp_n = 1;
+assign flash_reset_n = 1;
+
+spiflash spiflash_model (
+	.csb(flash_cs),
+	.clk(flash_clk),
+	.io0(flash_mosi),
+	.io1(flash_miso),
+	.io2(flash_wp_n),
+	.io3(flash_reset_n)
+);
+
 spi_flash_ctrl_top dut(
     .i_clk(clk),
     .i_btn(btn),
-    .i_spi_miso(zero)
+    .o_spi_cs_n(flash_cs),
+    .o_spi_clk(flash_clk),
+    .o_spi_mosi(flash_mosi),
+    .i_spi_miso(flash_miso)
 );
 
 
