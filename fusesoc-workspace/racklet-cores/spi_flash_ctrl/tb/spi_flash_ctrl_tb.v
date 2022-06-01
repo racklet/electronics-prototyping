8191326a2d1d2c09944803481ccc22ee75161a05
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2022-06-01 21:07:59 +0300

    WIP start modifying spi_flash_ctrl to internally buffer 1024 bytes

--- fusesoc-workspace/racklet-cores/spi_flash_ctrl/tb/spi_flash_ctrl_tb.v
+++ fusesoc-workspace/racklet-cores/spi_flash_ctrl/tb/spi_flash_ctrl_tb.v
@@ -1,5 +1,73 @@
 `timescale 1ns/1ns
 
+module wb_wrapper (
+    input  wire         wb_clk_i,
+	input  wire         wb_rst_i,
+
+	input  wire [31:0]  wb_adr_i,
+    output wire [31:0]  wb_dat_o,
+    input  wire [3:0]   wb_sel_i,
+	input  wire         wb_cyc_i,
+	input  wire         wb_stb_i,
+    input  wire         wb_we_i,
+    output reg          wb_ack_o
+);
+
+    // BRAM connections
+    wire       a_wr;
+    wire [9:0] a_addr;
+    wire [7:0] a_din;
+
+    wire [9:0] b_addr = 511;
+    wire [7:0] b_dout;
+    // assign PIN_10 = b_dout[1];
+
+    spi_flash_ctrl #(
+        .BLOCK_SIZE     ( 512 )
+    ) ctrl (
+        // Connections to system logic
+        .i_clk              ( i_clk      ),
+        .i_read_addr        ( 24'd0      ),
+        .i_read_stb         ( btn_down   ), // read enable (start read transaction)
+        .o_read_done_stb    ( PIN_10     ),
+
+        // Connections to BRAM
+        .o_write_bram_stb   ( a_wr       ),
+        .o_read_bram_addr   ( a_addr     ),
+        .o_read_bram_data   ( a_din      ),
+
+        // Connections to SPI flash
+        .o_spi_cs_n         ( o_spi_cs_n ),
+        .o_spi_clk          ( o_spi_clk  ),
+        .o_spi_mosi         ( o_spi_mosi ),
+        .i_spi_miso         ( i_spi_miso )
+    );
+
+    sd_bram_block_dp  #(
+        .DATA ( 8 ),    // byte addressable
+        .ADDR ( 9 )     // 512 locations
+    ) bram (
+        // Write port for data read from SPI flash
+        .a_clk      ( i_clk  ),
+        .a_wr       ( a_wr   ),
+        .a_addr     ( a_addr ),
+        .a_din      ( a_din  ),
+        // leave a_dout unconnected
+
+        // Read-only port for WB interface
+        .b_clk      ( i_clk  ),
+        .b_wr       ( 1'b0   ),  // read-only port 
+        .b_addr     ( wb_adr_i[9:0] ),
+        .b_dout     ( b_dout )
+        // leave b_din unconnected
+    );
+
+    assign wb_dat_o = {24'd0, b_dout};
+
+endmodule
+
+
+
 module spi_flash_ctrl_tb;
 
 vlog_tb_utils vtu();
---
+++ fusesoc-workspace/sourceme.sh
@@ -0,0 +1,1 @@
+export BOOTBLOB=`pwd`/bootblob.hex
